40bb2a422c1221f8c497e7e0ef32ea007814c1b2
nttneves <nttneves@gmail.com>
2026-02-03 17:52:21 +0000

    Implement Snake struct and movement logic

---
+++ src/snake.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct{
+  // Array to store the coordinates of snake
+  // tail (x-axis, y-axis)
+  int snakeTailX[100], snakeTailY[100];
+
+  // Variable to store the length of the
+  // snake's tail
+  int snakeTailLen;
+
+  // Coordinates of snake's head
+  int x, y;
+} Snake;
+
+void move_snake(Snake *s, int key){
+
+  int prevX = s->snakeTailX[0];
+  int prevY = s->snakeTailY[0];
+  int prev2X, prev2Y;
+  s->snakeTailX[0] = s->x;
+  s->snakeTailY[0] = s->y;
+
+  for(int i = 0; i < s->snakeTailLen; i++){
+    prev2X = s->snakeTailX[i];
+    prev2Y = s->snakeTailY[i];
+    s->snakeTailX[i] = prevX;
+    s->snakeTailY[i] = prevY;
+    prevX = prev2X;
+    prevY = prev2Y;
+  }
+
+  switch (key) {
+    case 1:
+        s->x--;
+        break;
+    case 2:
+        s->x++;
+        break;
+    case 3:
+        s->y--;
+        break;
+    case 4:
+        s->y++;
+        break;
+    default:
+        break;
+  }
+}
+
+void grow_snake(Snake *s){
+
+  if(s->snakeTailLen < 100){
+
+    int lastX = s->snakeTailX[s->snakeTailLen - 1];
+    int lastY = s->snakeTailY[s->snakeTailLen - 1];
+
+    s->snakeTailX[s->snakeTailLen] = lastX;
+    s->snakeTailY[s->snakeTailLen] = lastY;
+
+    s->snakeTailLen++;
+  }
+}
+
+bool check_collision(Snake *s){
+
+  for(int k = 0; k < s->snakeTailLen; k++){
+    if (s->x == s->snakeTailX[k] && s->y == s->snakeTailY[k]){
+      return true;
+    }
+  }
+
+  return false;
+} 
